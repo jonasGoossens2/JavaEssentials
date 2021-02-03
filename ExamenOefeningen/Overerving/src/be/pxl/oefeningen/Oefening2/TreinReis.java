592bda2dfb557912611f82f5e715191c8fc8b872
jonasGoossens2 <jonas.goossens2@student.pxl.be>
2021-02-03 12:53:40 +0100

    overerving

--- ExamenOefeningen/Overerving/src/be/pxl/oefeningen/Oefening2/TreinReis.java
+++ ExamenOefeningen/Overerving/src/be/pxl/oefeningen/Oefening2/TreinReis.java
@@ -42,4 +42,15 @@ public class TreinReis  extends Reis{
             this.specificatie = specificatie;
         }
     }
+
+    public String toString(){
+        String nationaal;
+        if(isNationaal()) {
+            nationaal = "Nationale";
+        }else{
+            nationaal = "internationale";
+        }
+        return String.format("Reis met bestemming %s kost %.2f euro\n%s treinreis (%s).",
+                getBestemming(), getPrijs(), nationaal, getSpecificatie());
+    }
 }
